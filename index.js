9848e05dca3ad08374639a59ca0533b6306c1d85
Siva-Tejaa <asivateja1999@gmail.com>
2024-02-24 13:09:54 +0530

    apis with basic swagger added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerDoc = require("swagger-jsdoc");
+
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -17,11 +21,46 @@ const notFoundRouter = require("./routers/notFoundRouter");
 
 const connectDB = require("./config/dbConfig");
 
-//cors for frontend access
-app.use(cors());
+//Security Packages
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const rateLimiter = require("express-rate-limit");
+
+//IP Limiter
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
+
+//Swagger API Config
+const options = {
+  definition: {
+    openapi: "3.0.3",
+    info: {
+      title: "Job Portal Application",
+      description: "descof application",
+    },
+    servers: [
+      {
+        url: "http://localhost:8080",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spec = swaggerDoc(options);
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(limiter);
 
 app.use("/", homeRouter);
 
@@ -32,6 +71,9 @@ app.use("/", testRouter);
 app.use("/", userRouter);
 
 app.use("/", jobsRouter);
+
+//Swagger Root HomePage
+app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(spec));
 
 app.use("*", notFoundRouter);
 
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -7,6 +7,76 @@ const { loginMiddleware } = require("../middlewares/loginMiddleware");
 
 const router = require("express").Router();
 
+/**
+ *
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type:Object
+ *      required:
+ *        - firstname
+ *        - lastname
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type:string
+ *          description: The Auto Generated ID of user collection
+ *          example:ASWEDRFTGYUHIKKJU8OL
+ *        firstname:
+ *          type:string
+ *          description: First Name
+ *        lastname:
+ *          type:string
+ *          description: Last Name
+ *        email:
+ *          type:string
+ *          description: Email
+ *        password:
+ *          type:string
+ *          description: Password
+ *      example:
+ *        id:ASWEDRFTGYUHIKKJU8OL
+ *        firstname:Siva
+ *        lastname:Teja
+ *        email:asivateja1999@gmail.com
+ *        password:SivaTeja@1999
+ *
+ */
+
+/**
+ *
+ * @swagger
+ * tags:
+ *  name:auth
+ *  description:Authentication API's
+ */
+
+/**
+ *
+ * @swagger
+ * /register
+ *  post:
+ *    summary:Register New User
+ *    tags:[Auth]
+ *    requestBody:
+ *    content:
+ *      application/json:
+ *        schema:
+ *          $ref:'#components/schemas/User'
+ *    responses:
+ *      201:
+ *        description:User Created Successfully
+ *  *     content:
+ *          application/json:
+ *            schema:
+ *              $ref:'#components/schemas/User'
+ *
+ *
+ */
+
+//REGISTER || POST
 router.post("/register", registerMiddleware, registerController);
 
 //LOGIN || POST
